f2fc175bb561d4359afd8b881941f4a1f19a7ab2
diego-rp <ing.diegorodriguez@gmail.com>
2020-01-29 02:02:34 -0500

    files 0-10

---
+++ run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+touch 0-current_working_directory
+touch 10-back
+touch 11-lists
+touch 12-file_type
+touch 13-symbolic_link
+touch 14-copy_html
+touch 15-lets_move
+touch 16-clean_emacs
+touch 17-tree
+touch 18-commas
+touch 1-listit
+touch 2-bring_me_home
+touch 3-listfiles
+touch 4-listmorefiles
+touch 5-listfilesdigitonly
+touch 6-firstdirectory
+touch 7-movethatfile
+touch 8-firstdelete
+touch 9-firstdirdeletion
+chmod a+x 0-current_working_directory
+chmod a+x 10-back
+chmod a+x 11-lists
+chmod a+x 12-file_type
+chmod a+x 13-symbolic_link
+chmod a+x 14-copy_html
+chmod a+x 15-lets_move
+chmod a+x 16-clean_emacs
+chmod a+x 17-tree
+chmod a+x 18-commas
+chmod a+x 1-listit
+chmod a+x 2-bring_me_home
+chmod a+x 3-listfiles
+chmod a+x 4-listmorefiles
+chmod a+x 5-listfilesdigitonly
+chmod a+x 6-firstdirectory
+chmod a+x 7-movethatfile
+chmod a+x 8-firstdelete
+chmod a+x 9-firstdirdeletion
+echo “#!/bin/bash” >> 0-current_working_directory
+echo “#!/bin/bash” >> 10-back
+echo “#!/bin/bash” >> 11-lists
+echo “#!/bin/bash” >> 12-file_type
+echo “#!/bin/bash” >> 13-symbolic_link
+echo “#!/bin/bash” >> 14-copy_html
+echo “#!/bin/bash” >> 15-lets_move
+echo “#!/bin/bash” >> 16-clean_emacs
+echo “#!/bin/bash” >> 17-tree
+echo “#!/bin/bash” >> 18-commas
+echo “#!/bin/bash” >> 1-listit
+echo “#!/bin/bash” >> 2-bring_me_home
+echo “#!/bin/bash” >> 3-listfiles
+echo “#!/bin/bash” >> 4-listmorefiles
+echo “#!/bin/bash” >> 5-listfilesdigitonly
+echo “#!/bin/bash” >> 6-firstdirectory
+echo “#!/bin/bash” >> 7-movethatfile
+echo “#!/bin/bash” >> 8-firstdelete
+echo “#!/bin/bash” >> 9-firstdirdeletion
+
