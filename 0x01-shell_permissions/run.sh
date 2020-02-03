be6f658dc3e848fa2741355f0ee316964901ae62
diego-rp <ing.diegorodriguez@gmail.com>
2020-02-03 11:54:17 -0500

    0x02

--- 0x01-shell_permissions/run.sh
+++ 0x01-shell_permissions/run.sh
@@ -1,53 +1,46 @@
 #!/bin/bash
-touch 0-iam_betty
-touch 1-who_am_i
-touch 2-groups
-touch 3-new_owner
-touch 4-empty
-touch 5-execute
-touch 6-multiple_permissions
-touch 7-everybody
-touch 8-James_Bond
-touch 9-John_Doe
-touch 10-mirror_permissions
-touch 11-directories_permissions
-touch 12-directory_permissions
-touch 13-change_group
-touch 14-change_owner_and_group
-touch 15-symbolic_link_permissions
-touch 16-if_only
-echo "#!/bin/bash" >> 0-iam_betty
-echo "#!/bin/bash" >> 1-who_am_i
-echo "#!/bin/bash" >> 2-groups
-echo "#!/bin/bash" >> 3-new_owner
-echo "#!/bin/bash" >> 4-empty
-echo "#!/bin/bash" >> 5-execute
-echo "#!/bin/bash" >> 6-multiple_permissions
-echo "#!/bin/bash" >> 7-everybody
-echo "#!/bin/bash" >> 8-James_Bond
-echo "#!/bin/bash" >> 9-John_Doe
-echo "#!/bin/bash" >> 10-mirror_permissions
-echo "#!/bin/bash" >> 11-directories_permissions
-echo "#!/bin/bash" >> 12-directory_permissions
-echo "#!/bin/bash" >> 13-change_group
-echo "#!/bin/bash" >> 14-change_owner_and_group
-echo "#!/bin/bash" >> 15-symbolic_link_permissions
-echo "#!/bin/bash" >> 16-if_only
-chmod 777 0-iam_betty
-chmod 777 1-who_am_i
-chmod 777 2-groups
-chmod 777 3-new_owner
-chmod 777 4-empty
-chmod 777 5-execute
-chmod 777 6-multiple_permissions
-chmod 777 7-everybody
-chmod 777 8-James_Bond
-chmod 777 9-John_Doe
-chmod 777 10-mirror_permissions
-chmod 777 11-directories_permissions
-chmod 777 12-directory_permissions
-chmod 777 13-change_group
-chmod 777 14-change_owner_and_group
-chmod 777 15-symbolic_link_permissions
-chmod 777 16-if_only
-
+touch 0-alias
+touch 1-hello_you
+touch 2-path
+touch 3-paths
+touch 4-global_variables
+touch 5-local_variables
+touch 6-create_local_variable
+touch 7-create_global_variable
+touch 8-true_knowledge
+touch 9-divide_and_rule
+touch 10-love_exponent_breath
+touch 11-binary_to_decimal
+touch 12-combinations
+touch 13-print_float
+touch 14-decimal_to_hexadecimal
+echo "#!/bin/bash" >> 0-alias
+echo "#!/bin/bash" >> 1-hello_you
+echo "#!/bin/bash" >> 2-path
+echo "#!/bin/bash" >> 3-paths
+echo "#!/bin/bash" >> 4-global_variables
+echo "#!/bin/bash" >> 5-local_variables
+echo "#!/bin/bash" >> 6-create_local_variable
+echo "#!/bin/bash" >> 7-create_global_variable
+echo "#!/bin/bash" >> 8-true_knowledge
+echo "#!/bin/bash" >> 9-divide_and_rule
+echo "#!/bin/bash" >> 10-love_exponent_breath
+echo "#!/bin/bash" >> 11-binary_to_decimal
+echo "#!/bin/bash" >> 12-newest_create_global_variables
+echo "#!/bin/bash" >> 13-print_float
+echo "#!/bin/bash" >> 14-decimal_to_hexadecimal
+chmod 755 0-alias
+chmod 755 1-hello_you
+chmod 755 2-path
+chmod 755 3-paths
+chmod 755 4-global_variables
+chmod 755 5-local_variables
+chmod 755 6-create_local_variable
+chmod 755 7-create_global_variable
+chmod 755 8-true_knowledge
+chmod 755 9-divide_and_rule
+chmod 755 10-love_exponent_breath
+chmod 755 11-binary_to_decimal
+chmod 755 12-newest_create_global_variables
+chmod 755 13-print_float
+chmod 755 14-decimal_to_hexadecimal
---
+++ 0x03-shell_variables_expansions/run.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+touch 0-hello_world
+touch 1-confused_smiley
+touch 2-hellofile
+touch 3-twofiles
+touch 4-lastlines
+touch 5-firstlines
+touch 6-third_line
+touch 7-file
+touch 8-cwd_state
+touch 9-duplicate_last_line
+touch 10-no_more_js
+touch 11-directories
+touch 12-newest_files
+touch 13-unique
+touch 14-findthatword
+touch 15-countthatword
+touch 16-whatsnext
+touch 17-hidethisword
+touch 18-letteronly
+touch 19-AZ
+touch 20-hiago
+touch 21-reverse
+touch 22-users_and_homes
+echo "#!/bin/bash" >> 0-hello_world
+echo "#!/bin/bash" >> 1-confused_smiley
+echo "#!/bin/bash" >> 2-hellofile
+echo "#!/bin/bash" >> 3-twofiles
+echo "#!/bin/bash" >> 4-lastlines
+echo "#!/bin/bash" >> 5-firstlines
+echo "#!/bin/bash" >> 6-third_line
+echo "#!/bin/bash" >> 7-file
+echo "#!/bin/bash" >> 8-cwd_state
+echo "#!/bin/bash" >> 9-duplicate_last_line
+echo "#!/bin/bash" >> 10-no_more_js
+echo "#!/bin/bash" >> 11-directories
+echo "#!/bin/bash" >> 12-newest_files
+echo "#!/bin/bash" >> 13-unique
+echo "#!/bin/bash" >> 14-findthatword
+echo "#!/bin/bash" >> 15-countthatword
+echo "#!/bin/bash" >> 16-whatsnext
+echo "#!/bin/bash" >> 17-hidethisword
+echo "#!/bin/bash" >> 18-letteronly
+echo "#!/bin/bash" >> 19-AZ
+echo "#!/bin/bash" >> 20-hiago
+echo "#!/bin/bash" >> 21-reverse
+echo "#!/bin/bash" >> 22-users_and_homes
+chmod 755 0-hello_world
+chmod 755 1-confused_smiley
+chmod 755 2-hellofile
+chmod 755 3-twofiles
+chmod 755 4-lastlines
+chmod 755 5-firstlines
+chmod 755 6-third_line
+chmod 755 7-file
+chmod 755 8-cwd_state
+chmod 755 9-duplicate_last_line
+chmod 755 10-no_more_js
+chmod 755 11-directories
+chmod 755 12-newest_files
+chmod 755 13-unique
+chmod 755 14-findthatword
+chmod 755 15-countthatword
+chmod 755 16-whatsnext
+chmod 755 17-hidethisword
+chmod 755 18-letteronly
+chmod 755 19-AZ
+chmod 755 20-hiago
+chmod 755 21-reverse
+chmod 755 22-users_and_homes
+
+
